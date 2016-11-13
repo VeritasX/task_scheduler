3623761257c3f7717d554eadc24d7a37c500baab
Aaron Endsley <aaronendsley@gmail.com>
2016-11-12 18:48:52 -0600

    added a checkFunction to double check if there is a double

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 var tasksList={};
 
 var submitTasks=function(){
-var taskName = $('#taskName').val(), taskTime = $('#taskTime').val(), taskDay = $('#taskDay').val();
+var taskName = $('#taskName').val(), taskTime = $('#taskTime').val(), taskDay = $('#taskDay').val(), thereIsADouble;
 function moveItemsToList(taskTitle){
  tasksList[taskTitle] =  {
         task:taskName,
@@ -12,14 +12,25 @@ function moveItemsToList(taskTitle){
 
 var checkForDoubleDates = function(){
     for(var listItem in tasksList){
-        if(tasksList[listItem]['taskDate'] === taskDay || tasksList[listItem]['taskTime']  === taskTime){
-            console.log('it is the same as ' + tasksList[listItem]['task'])
+        if(tasksList[listItem]['taskDate'] === taskDay && tasksList[listItem]['taskTime']  === taskTime){
+            console.log('it is the same as ' + tasksList[listItem]['task']);
+            $('#alerts').append('<p>You already have the task "' + tasksList[listItem]['task'] + '" at that day and time!  </p>');
+            thereIsADouble = {'double':true};
         }
     }
 };
+var verifyCheckForDoubleDates =function(){
+if(thereIsADouble){
+    console.log('sorry there was a double');
+}else{
+    moveItemsToList(taskName);
+}
+
+}
 
 checkForDoubleDates();
-moveItemsToList(taskName);
+verifyCheckForDoubleDates();
+ 
 console.log(tasksList);
 };
 
